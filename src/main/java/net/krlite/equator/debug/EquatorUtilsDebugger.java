19312ac4fc0f5fe3023437d3151a56df5a959d08
KrLite <68179735+KrLite@users.noreply.github.com>
2023-01-24 20:25:14 +0800

    delete debug

--- src/main/java/net/krlite/equator/debug/EquatorUtilsDebugger.java
+++ src/main/java/net/krlite/equator/debug/EquatorUtilsDebugger.java
@@ -8,11 +8,5 @@ import java.text.DecimalFormat;
 @SuppressWarnings("allJavadoc")
 public class EquatorUtilsDebugger {
 	public static void main(String[] args) {
-		Timer t = new Timer(100000);
-		EasingFunctions.Combined c = new EasingFunctions.Combined().append(EasingFunctions.Quadratic::easeOut, 5).appendNegate(EasingFunctions.Bounce::easeOut, 15);
-		while (true) {
-			t.run(t::reset);
-			System.out.println(new DecimalFormat("#.###").format(c.apply(t)));
-		}
 	}
 }
